9f946e4a330128eaaa478ca8d5922e154a2f8a1e
Shengda <dingshengda@gmail.com>
2020-12-19 09:59:09 -0800

    Add

--- script/deploy_vault_proxy.js
+++ script/deploy_vault_proxy.js
@@ -7,7 +7,7 @@ module.exports = async function (callback) {
         const vault = (await Vault.deployed()).address;
         console.log('Vault address: ' + vault);
         
-        const vaultProxy = await deployer.deploy(AdminUpgradeabilityProxy, vault, argv.account);
+        const vaultProxy = await AdminUpgradeabilityProxy.new(vault, argv.account);
         const proxiedVault = await Vault.at(vaultProxy.address);
 
         await proxiedVault.initialize(argv.token, argv.controller, '', '');
