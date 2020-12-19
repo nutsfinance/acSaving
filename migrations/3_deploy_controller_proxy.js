da9334df3140e3aa0e268f076d1c67844701e657
Shengda <dingshengda@gmail.com>
2020-12-19 11:54:52 -0800

    Add proxies to controller and saving application

--- migrations/3_deploy_controller_proxy.js
+++ migrations/3_deploy_controller_proxy.js
@@ -1,21 +1,22 @@
 const Controller = artifacts.require("Controller");
-const AdminUpgradeabilityProxy = artifacts.require("AdminUpgradeabilityProxy");
+const ControllerProxy = artifacts.require("ControllerProxy");
 
 const deployControllerProxy = async (deployer, accounts) => {
     const controller = (await Controller.deployed()).address;
     console.log('Controller address: ' + controller);
     console.log('Proxy admin: ' + accounts[0]);
 
-    const controllerProxy = await deployer.deploy(AdminUpgradeabilityProxy, controller, accounts[0]);
+    const controllerProxy = await deployer.deploy(ControllerProxy, controller, accounts[0]);
     const proxiedController = await Controller.at(controllerProxy.address);
+    console.log('Controller proxy address: ' + controllerProxy.address);
 
     // Beta
-    const ac = "0xD104F7479117209c1B885390500f29110f84E8FB";
-    const acDAO = "0x4804E40de5E8d06d7a37e2b6e417Fe92cCd0F677";
+    // const ac = "0xD104F7479117209c1B885390500f29110f84E8FB";
+    // const acDAO = "0x4804E40de5E8d06d7a37e2b6e417Fe92cCd0F677";
     // Production
-    // const ac = "0x9A0aBA393aac4dFbFf4333B06c407458002C6183";
-    // const acDAO = "0xc25D6AD0C82F21bE056699d575284e18678F8fE5";
-    await proxiedController.initialize(ac, acDAO);
+    const aCoconut = "0x9A0aBA393aac4dFbFf4333B06c407458002C6183";  // reward token
+    const aCoconutDAO = "0xc25D6AD0C82F21bE056699d575284e18678F8fE5";   // treasury
+    await proxiedController.initialize(aCoconut, aCoconutDAO);
 }
 
 module.exports = function (deployer, network, accounts) {
--- migrations/6_deploy_saving_application_proxy.js
+++ migrations/6_deploy_saving_application_proxy.js
@@ -1,18 +1,18 @@
 const SavingApplication = artifacts.require("SavingApplication");
-const AdminUpgradeabilityProxy = artifacts.require("AdminUpgradeabilityProxy");
+const SavingApplicationProxy = artifacts.require("SavingApplicationProxy");
+const ControllerProxy = artifacts.require("ControllerProxy");
 
 const deploySavingApplicationProxy = async (deployer, accounts) => {
     const savingApplication = (await SavingApplication.deployed()).address;
     console.log('Saving application address: ' + savingApplication);
     console.log('Proxy admin: ' + accounts[0]);
 
-    const savingApplicationProxy = await deployer.deploy(AdminUpgradeabilityProxy, savingApplication, accounts[0]);
+    const savingApplicationProxy = await deployer.deploy(SavingApplicationProxy, savingApplication, accounts[0]);
     const proxiedSavingApplication = await SavingApplication.at(savingApplicationProxy.address);
+    console.log('Saving application proxy: ' + savingApplicationProxy.address);
 
-    // Beta
-    const controller = "0xE0127d51b64400D5400740a633EB705611d99822";
-    // Production
-    // const controller = "";
+    const controller = (await ControllerProxy.deployed()).address();
+    console.log('Controller: ' + controller);
     await proxiedSavingApplication.initialize(controller);
 }
 
